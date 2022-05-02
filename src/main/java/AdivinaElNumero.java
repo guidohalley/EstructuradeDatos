745d00a1c1b962ff3eed9445c13221200e55f4ee
guidohalley <guidohalley@hotmail.com>
2022-05-02 19:17:50 -0300

    Creando el juego, Creando el Proyecto - #11

---
+++ src/main/java/AdivinaElNumero.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AdivinaElNumero {
+    private  Scanner entrada = new Scanner(System.in);
+    private boolean juegoActivo = true;
+
+
+    public void jugar(){
+        System.out.println("Hola, cual es tu nombre ?");
+        String nombreJugador = entrada.nextLine();
+        System.out.printf("Bienvenido %s, vamos a comenzar \n",nombreJugador);
+
+        while (juegoActivo){
+            int min = 0;
+            int max = 100;
+
+            int numeroJuego = (int) (Math.random() * ((max - min) + 1));
+
+            System.out.println(numeroJuego);
+            juegoActivo = false;
+
+        }
+
+    }
+}
